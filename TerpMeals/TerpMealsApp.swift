6443215372e65cc13730a618934a67ce87312f7d
Krisnajit Rajeshkhanna <goldenkrishna123@gmail.com>
2024-06-18 22:57:31 -0400

    Initial Commit

---
+++ TerpMeals/TerpMealsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TerpMealsApp.swift
+//  TerpMeals
+//
+//  Created by krisnajit Rajeshkhanna on 6/18/24.
+//
+
+import SwiftUI
+
+@main
+struct TerpMealsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
